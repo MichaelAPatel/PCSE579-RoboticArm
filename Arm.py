f4d5383aa57348cf82b8cfc5d12ea214609ddde2
michael.patel <michael.patel@LIGO.org>
2019-11-25 13:05:11 -0500

    Chnages to UI and arm motion calculations.

--- Arm.py
+++ Arm.py
@@ -4,14 +4,43 @@ import sys
 import warnings
 from scipy.integrate import odeint
 from scipy.optimize import fsolve
-from matplotlib.pyplot import figure, plot, legend, xlabel, ylabel, show
+from matplotlib.pyplot import figure, plot, show, grid, Circle, figtext
 warnings.filterwarnings('error')
+
+# ============================================Snapshot Information==================================================== #
+# First Quadrant Out
+r1 = 5.0
+theta1 = 154.5
+r2 = 4.8
+theta2 = 153.86
+
+# Second Quadrant Strike
+# r1 = 5.0
+# theta1 = 25.5
+# r2 = 4.8
+# theta2 = 26.14
+
+# Third Quadrant Out
+# r1 = 5.0
+# theta1 = 25.5
+# r2 = 4.8
+# theta2 = 25.9
+
+# Fourth Quadrant Out
+# r1 = 5.0
+# theta1 = 25.5
+# r2 = 4.8
+# theta2 = 25.7
+# ==================================================================================================================== #
 
 # Define constants, note that moment of inertia will probably change.
 mass = 0.03
 radius = 0.6
 g = 9.81
 i = 0.0036
+tau = 5
+
+annotateString = ''
 
 
 # Method to with equations that represent the balls trajectory and the catchable region. Used by FSolve to find
@@ -29,23 +58,9 @@ def paths(z, x0, y0, vx, vy0):
 def armmotion(vect, t):
     theta = vect[0]
     theta_dot = vect[1]
-    tau = -5
-    theta_ddot = (tau - (mass * g * radius * numpy.sin(theta))) / (i + mass * pow(radius, 2))
+    theta_ddot = (tau - (mass * g * (radius / 2) * numpy.sin(theta))) / (i + mass * pow(radius / 2, 2))
     return [theta_dot, theta_ddot]
 
-
-# Data from the two snapshots where angles are in degrees (Change as needed)
-#Set 1
-# r1 = 5.0
-# theta1 = 225.13
-# r2 = 3.8075
-# theta2 = 225.32
-
-#Set 2
-r1 = 5.0
-theta1 = 221.62
-r2 = 3.8233
-theta2 = 219.14
 
 # Start the calculation clock.
 start_time = time.time()
@@ -79,13 +94,27 @@ for i in intersectionGuesses:
 # If there are no intersection points it is a ball and we can exit the program.
 if not xint:
     stop_time = time.time()
-    print('BALL!')
-    print('\n')
-    print('Initial X Position: ' + str(x2))
-    print('Initial Y Position: ' + str(y2))
-    print('Calcuated X Velocity: ' + str(vx))
-    print('Calculated Initial Y Velocity: ' + str(vy0))
-    print('Calculations completed in ' + str(stop_time - start_time) + 's.')
+    annotateString += 'BALL!'
+    annotateString += '\nInitial X Position: ' + str(round(x2, 3))
+    annotateString += '\nInitial Y Position: ' + str(round(y2, 3))
+    annotateString += '\nCalculated X Velocity: ' + str(round(vx, 3))
+    annotateString += '\nCalculated Initial Y Velocity: ' + str(round(vy0, 3))
+    annotateString += '\nCalculations completed in ' + str(round(stop_time - start_time, 3)) + 's.'
+
+    arm = Circle((0, 0), 0.6, color='b', fill=False)
+    fig = figure(num=0.0, figsize=(6, 6))
+    fig.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.3)
+    ax = fig.add_subplot(111, aspect='equal')
+    grid(b=True, which='major', axis='both')
+    ax.add_artist(arm)
+    ax.set_xticks([-1.5, -1, -0.5, 0.5, 1, 1.5])
+    ax.set_yticks([-1.5, -1, -0.5, 0.5, 1, 1.5])
+    ax.spines['left'].set_position('center')
+    ax.spines['bottom'].set_position('center')
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    figtext(0.0, 0.0, annotateString)
+    show()
     sys.exit(0)
 
 # Calculate the time for each intersection point based upon the x velocity which is constant.
@@ -104,23 +133,23 @@ if 0 <= angle <= numpy.pi / 2:
     armangle = angle + (numpy.pi / 2)
 # Second Quadrant
 elif numpy.pi / 2 <= angle <= numpy.pi:
-    armangle = 3 * numpy.pi / 2 - angle
-# Third Quadrant
+    armangle = -1.0 * (3 * numpy.pi / 2 - angle)
+# Fourth Quadrant
 elif -1.0 * numpy.pi / 2 <= angle <= 0:
     armangle = angle + (numpy.pi / 2)
-# Fourth Quadrant
+# Third Quadrant
 elif -1.0 * numpy.pi <= angle <= -1.0 * numpy.pi / 2:
-    armangle = numpy.abs(angle + (numpy.pi / 2))
+    armangle = angle + (numpy.pi / 2)
 
 # Calculate arm motion by numerically solving the arm's equation of motion up to the max_time.
-t = numpy.linspace(0.0, max_time, 101)
+t = numpy.linspace(0.0, max_time, 100)
 initialcondition = [0.0, 0.0]
 solution = odeint(armmotion, initialcondition, t)
 
 # Check when, if ever, the arm reaches the necessary angle.
 arm_time = pow(10, 17)
 for i in range(len(solution)):
-    if solution[i][0] >= armangle:
+    if numpy.abs(solution[i][0]) >= numpy.abs(armangle):
         arm_time = t[i]
         break
 
@@ -129,30 +158,40 @@ stop_time = time.time()
 total_time = stop_time - start_time + arm_time
 
 if total_time > max_time:
-    print('STRIKE!')
+    annotateString = 'STRIKE!'
 else:
-    print('OUT!')
+    annotateString = 'OUT!'
 
-print('\n')
-print('Initial X Position: ' + str(x2))
-print('Initial Y Position: ' + str(y2))
-print('Calculated X Velocity: ' + str(vx))
-print('Calculated Initial Y Velocity: ' + str(vy0))
-print('Angles of intersection:')
+annotateString += '\n'
+annotateString += '\nInitial X Position: ' + str(round(x2, 3))
+annotateString += '\nInitial Y Position: ' + str(round(y2, 3))
+annotateString += '\nCalculated X Velocity: ' + str(round(vx, 3))
+annotateString += '\nCalculated Initial Y Velocity: ' + str(round(vy0, 3))
+annotateString += '\nAngles of intersection:'
 for i in angleint:
-    print('    ' + str(i * 180 / numpy.pi))
-print('The program selected the angle ' + str(armangle * 180 / numpy.pi) + '. It must reach this angle in '
-      + str(max_time) + ' to catch the ball. The arm was only able to reach ' + str(solution[len(solution)-1][0] * 180
-      / numpy.pi) + '.')
-print('Calculations completed in ' + str(stop_time - start_time) + 's.')
+    annotateString += '\n    ' + str(round(i * 180 / numpy.pi, 3))
+if total_time > max_time:
+    annotateString += '\nThe program selected the angle ' + str(round(armangle * 180 / numpy.pi, 3)) + '. It must reach this angle in ' + str(round(max_time, 3)) + ' s. \n The arm was only able to reach ' + str(round(solution[len(solution) - 1][0] * 180 / numpy.pi, 3)) + ' in that time.'
+else:
+    annotateString += '\nThe program selected the angle ' + str(round(armangle * 180 / numpy.pi, 3)) + '. It must reach this angle in ' + str(round(max_time, 3)) + ' s. \n The arm was able to reach ' + str(round(armangle * 180 / numpy.pi, 3)) + ' in ' + str(round(arm_time,3)) + 's.'
 
-theta = []
-for i in solution:
-    theta.append(i[0]*180/numpy.pi)
+annotateString += '\nCalculations completed in ' + str(round(stop_time - start_time, 3)) + 's.'
 
-figure()
-plot(t, theta)
-legend(('θ'))
-xlabel('TIME (sec)')
-ylabel('θ(deg)')
+# Graph of Ball Trajectory
+arm = Circle((0, 0), 0.6, color='b', fill=False)
+x = numpy.linspace(-1, 1, 100)
+y = y2 + vy0 * (x - x2) / vx - 4.905 * pow((x - x2) / vx, 2)
+fig = figure(num=0.0, figsize=(6, 6))
+fig.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.3)
+ax = fig.add_subplot(111, aspect='equal')
+grid(b=True, which='major', axis='both')
+plot(x, y)
+ax.add_artist(arm)
+ax.set_xticks([-1.5, -1, -0.5, 0.5, 1, 1.5])
+ax.set_yticks([-1.5, -1, -0.5, 0.5, 1, 1.5])
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position('center')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+figtext(0.0, 0.0, annotateString)
 show()
